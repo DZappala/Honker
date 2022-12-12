06996010a3e784f30f279a6d1c7e870c76f8faeb
DZappala <dominic.zappala@gmail.com>
2022-12-12 17:50:11 -0500

    feat: Navbar

---
+++ honker/app.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="container h-screen">
+    <!-- 50 is the highest z-index -->
+    <Navbar class="sticky top-0 z-50" />
+  </div>
+</template>
+
+<style>
+/* Force the app to always center */
+body {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
