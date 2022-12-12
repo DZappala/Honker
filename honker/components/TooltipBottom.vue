ba7bcb42734b35c6a51bc01cf2f8d0304aac2877
DZappala <dominic.zappala@gmail.com>
2022-12-12 17:54:11 -0500

    feat: companion images

---
+++ honker/components/TooltipBottom.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const props = defineProps<{
+  tooltipMessage: string;
+  isShown?: boolean;
+}>();
+</script>
+<template>
+  <div
+    :class="{ 'tooltip tooltip-open tooltip-bottom': isShown }"
+    :data-tip="tooltipMessage"
+  >
+    <slot />
+  </div>
+</template>
