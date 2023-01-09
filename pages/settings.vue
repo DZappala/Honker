ebeaffa06121ac0b7fde9391fceba397af920c99
DZappala <dominic.zappala@gmail.com>
2023-01-09 12:30:33 -0500

    feat: location search

--- pages/settings.vue
+++ pages/settings.vue
@@ -1,31 +1,53 @@
 <script setup lang="ts">
-import { SupabaseClient } from "@supabase/supabase-js";
+import type { Database } from "~/types/database.types";
 
-const supabase: SupabaseClient = useSupabaseClient();
+const supabase = useSupabaseClient<Database>();
 const user = useSupabaseUser();
+const router = useRouter();
 
 const loading = ref(true);
 const username = ref("");
 const website = ref("");
-const avatar_path = ref("");
+const bio = ref("");
+const avatar_url = ref("");
+const location = ref("");
+const locationPredictions = ref<string[]>([]);
+const unsavedChanges = ref(false); //implement save changes state
+const characterCount = computed(() => {
+  return 256 - bio.value.length;
+});
 
 loading.value = true;
 const { data } = await supabase
   .from("users")
-  .select(`username, website, avatar_url`)
+  .select("*")
   .eq("id", user.value?.id)
   .single();
 
 if (data) {
   username.value = data.username;
-  website.value = data.website;
-  avatar_path.value = data.avatar_url;
+  website.value = data.website || "";
+  bio.value = data.bio || "";
+  avatar_url.value = data.avatar_url || "";
+  location.value = data.location || "";
 }
 loading.value = false;
+
+//TODO: if the user has entered text into any of the fields and hasn't pressed update, warn them before they leave the page
+
+const validateBio = () => {
+  if (bio.value.length > 256) {
+    throw new Error("Bio cannot be longer than 256 characters");
+  }
+  if (!bio.value || bio.value == "") {
+    throw new Error("Bio cannot be empty");
+  }
+};
 
 const updateUser = async () => {
   try {
     loading.value = true;
+    validateBio(); //TODO: add other validations
 
     const { error } = await supabase
       .from("users")
@@ -33,7 +55,9 @@ const updateUser = async () => {
         id: user.value?.id,
         username: username.value,
         website: website.value,
-        avatar_url: avatar_path.value,
+        bio: bio.value,
+        location: location.value,
+        avatar_url: avatar_url.value,
         updated_at: new Date().toISOString(),
       })
       .eq("id", user.value?.id);
@@ -57,14 +81,70 @@ const signOut = async () => {
   } catch (error: any | unknown) {
     alert(error.message);
   } finally {
+    alert("You have been logged out!");
+    router.push("/auth");
     loading.value = false;
   }
 };
+
+const getLocationPredictions = async () => {
+  const { data, pending, error, refresh } = await useFetch<locationQueryResult>(
+    `https://public.opendatasoft.com/api/v2/catalog/datasets/geonames-all-cities-with-a-population-1000/records?limit=5&offset=0&select=name,cou_name_en&where=search(name,"${location.value}")&order_by=population DESC`
+  );
+
+  if (pending) {
+    locationPredictions.value = ["Loading ..."];
+  }
+
+  if (error.value) {
+    throw new Error(error.value.message);
+  }
+
+  locationPredictions.value =
+    data?.value?.records.map(
+      (record) =>
+        `${record.record.fields.name}, ${record.record.fields.cou_name_en}`
+    ) || [];
+};
+
+watch(location, async () => {
+  if (location.value.length < 3) {
+    locationPredictions.value = [];
+    return;
+  }
+  await getLocationPredictions().catch((error) => {
+    console.error(error);
+  });
+});
+
+//TODO: implement more robust location search with city, country, etc.
+// interface queryBuilderWhereOpts {
+//   filter: string[];
+//   combine: "AND" | "OR";
+// }
+
+// const locationQueryBuilder = (
+//   uri: string,
+//   params: {
+//     limit?: number;
+//     offset?: number;
+//     select?: string;
+//     where?: string | queryBuilderWhereOpts;
+//   }
+// ) => {
+//   if (typeof params.where === "object") {
+//     const { filter, combine } = params.where;
+//     params.where = filter.join(` ${combine} `);
+//   }
+//   return `${uri}?${Object.entries(params)
+//     .map(([key, value]) => `${key}=${value}`)
+//     .join("&")}`;
+// };
 </script>
 
 <template>
   <form
-    class="form-control h-screen container mx-auto flex flex-col justify-center items-center prose gap-4 w-1/3"
+    class="form-control h-screen container mx-auto flex flex-col justify-center items-center prose gap-4 w-full p-6 sm:p-0"
     @submit.prevent="updateUser"
   >
     <input
@@ -83,16 +163,61 @@ const signOut = async () => {
         id="username"
         type="text"
         v-model="username"
+        placeholder="choose a username"
       />
     </label>
 
-    <label class="input-group input-group-vertical" for="username">
+    <label class="input-group input-group-vertical" for="website">
       <span>Website</span>
       <input
         class="input input-bordered"
-        id="username"
+        id="website"
         type="text"
         v-model="website"
+        placeholder="link your website"
+      />
+    </label>
+
+    <label class="input-group input-group-vertical" for="location">
+      <span>Location</span>
+      <input
+        class="input input-bordered"
+        id="location"
+        type="text"
+        autocomplete="off"
+        autocorrect="off"
+        v-model="location"
+        placeholder="where are you?"
+      />
+      <div class="dropdown dropdown-open" v-if="locationPredictions.length > 0">
+        <ul
+          tabindex="0"
+          class="dropdown-content menu p-2 shadow bg-base-300 text-base-content rounded-box"
+        >
+          <li v-for="prediction in locationPredictions" :key="prediction">
+            <span @click="location = prediction">{{ prediction }}</span>
+          </li>
+        </ul>
+      </div>
+    </label>
+
+    <label class="input-group input-group-vertical" for="bio">
+      <span
+        :class="{
+          'text-error': characterCount < 0,
+        }"
+        >{{ characterCount }}</span
+      >
+      <textarea
+        id="bio"
+        type="text"
+        v-model="bio"
+        placeholder="write a bio!"
+        class="textarea resize-none w-full"
+        :class="{
+          'textarea-error': characterCount < 0,
+          'textarea-bordered': characterCount >= 0,
+        }"
       />
     </label>
 
@@ -103,7 +228,7 @@ const signOut = async () => {
         :value="loading ? 'Loading ...' : 'Update'"
         :disabled="loading"
       />
-      <button class="btn btn-warning" @click="signOut" :disabled="loading">
+      <button class="btn btn-error" @click="signOut" :disabled="loading">
         Sign Out
       </button>
     </div>
